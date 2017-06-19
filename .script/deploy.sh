07ae96ca009be8d019915647e49270ab8196bd3f
Turbo <thinhturbo@gmail.com>
2017-06-19 06:11:20 +0200

    Travis: Attemp 7

--- .script/deploy.sh
+++ .script/deploy.sh
@@ -2,16 +2,16 @@
 set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
     # Initialize a new git repo in _site, and push it to our server.
-    mkdir _site
+    ls
     cd _site
+    ls
     git init
 
     ssh-keyscan 45.79.161.79 >> ~/.ssh/known_hosts
-    git remote add deploy "turbo@45.79.161.79:/var/www/public"
+    git remote add deploy "turbo@45.79.161.79:/var/www/home"
     git config user.name "Turbo"
     git config user.email "tienthinhturbo@gmail.com"
     
-    ls ../
     git add ../dist
     git commit -m "Deploy"
     git push --force deploy master
